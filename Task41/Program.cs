5174bdb8bf8a6997a1118cfc1dfa7f6e3543a397
Kate <ekatromanova90@mail.ru>
2022-10-19 21:08:46 +0300

    hometasks

---
+++ Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+
+int PositElem (int m)
+{
+    int count = 0;
+    for (int i = 0; i < m; i++)
+    {
+        Console.WriteLine("Введите число под номером "+(i+1));
+        int n = Convert.ToInt32(Console.ReadLine());
+        if(n > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine("Введите количество чисел: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Kоличество чисел больше нуля: "+PositElem(N));
---
+++ Task43/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+void Point(double a, double b, double c, double d)
+{
+    if(a==c)
+    {
+        Console.WriteLine("Прямые параллельны");
+    }
+    else
+    {
+        double X =(d-b)/(a-c);
+        double Y =a*X+b;
+        Console.WriteLine("Точка пересечения прямой у="+a+"x+"+b+" и прямой у="+c+"x+"+d+": ("+X+";"+Y+")");
+    }
+}
+Console.WriteLine("Введите угловой коэффициент К первой прямой: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите свободный член В первой прямой: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите угловой коэффициент К второй прямой: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите свободный член В второй прямой: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Point(k1,b1,k2,b2);
